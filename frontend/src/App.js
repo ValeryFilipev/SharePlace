2067a6e8b56ee37644388b8b2644060ebb2bb70a
Valery Filipev <valeranuzdin1998@mail.ru>
2020-02-29 16:09:15 +0300

    users list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlaces from './places/pages/NewPlace';
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlace";
 
 const App = () => {
   return (
-      <Router>
-          <Switch>
-            <Route path="/" exact>
-              <Users />
-            </Route>
-              <Route path="/places/new" exact>
-                  <NewPlaces />
-              </Route>
-            <Redirect to="/" />
-          </Switch>
-      </Router>
-  )
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlaces />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
