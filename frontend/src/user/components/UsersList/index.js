ebb8ca8b5ac1b14e4278ec57de240038dc163f9d
Valery Filipev <valeranuzdin1998@mail.ru>
2020-03-15 01:10:21 +0300

    add main header with responsive design

--- frontend/src/user/components/UsersList/index.js
+++ frontend/src/user/components/UsersList/index.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 import UsersItem from "../UserItem";
+import Card from "../../../shared/components/UI/Card";
 
 import "./index.css";
 
@@ -7,7 +9,9 @@ const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
