28f738b9d1386933d4e2da2046ed6b92b39c959a
Valery Filipev <valeranuzdin1998@mail.ru>
2020-04-07 13:05:04 +0300

    multiple inputs in Place form with validity

--- frontend/src/places/pages/NewPlace/index.js
+++ frontend/src/places/pages/NewPlace/index.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../../shared/components/UI/Form/Input";
-import { VALIDATOR_REQUIRE } from "../../../shared/util/validators";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../../shared/util/validators";
 
 import "./index.css";
 
-const NewPlace = () => (
-  <form className="place-form">
-    <Input
-      element="input"
-      type="text"
-      label="Title"
-      validators={[VALIDATOR_REQUIRE()]}
-      errorText="Please enter a valid title."
-    />
-  </form>
-);
+const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description. (at least 5 characters)."
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
+};
 
 export default NewPlace;
