bf6c7ad2b24c8cc319c3aa48931763fe3b885e02
Valery Filipev <valeranuzdin1998@mail.ru>
2020-04-28 20:57:47 +0300

    add post route using postman from the body

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -1,3 +1,5 @@
+const uuid = require("uuid/v4");
+
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -5,13 +7,11 @@ const DUMMY_PLACES = [
     id: "p1",
     title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
     location: {
-      lat: 40.7484405,
-      lng: -73.9878584
+      lat: 40.7484474,
+      lng: -73.9871516
     },
+    address: "20 W 34th St, New York, NY 10001",
     creator: "u1"
   }
 ];
@@ -42,5 +42,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
