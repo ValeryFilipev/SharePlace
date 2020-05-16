c67da96208b6d72f908b5f35dadb20e2d7acaefc
Valery Filipev <valeranuzdin1998@mail.ru>
2020-05-16 13:16:11 +0300

    use google map instead of source map

--- frontend/src/components/UI/Map/index.js
+++ frontend/src/components/UI/Map/index.js
@@ -31,7 +31,26 @@ const Map = props => {
   //     id="map"
   //   />
   // );
-  
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center,
+      zoom
+    });
+
+    new window.google.maps.Marker({ position: center, map });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    />
+  );
 };
 
 Map.propTypes = {
