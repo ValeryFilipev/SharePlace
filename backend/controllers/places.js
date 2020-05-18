517ca1da86e00b15689f63b6ea4d929072e9b072
Valery Filipev <valeranuzdin1998@mail.ru>
2020-05-18 17:31:21 +0300

    403 - for invalid credentials
    add auth for edit and delete place - only exact user can do it

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -140,6 +140,14 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError(
+      "You are not allowed to edit this place.",
+      401
+    );
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -172,6 +180,14 @@ const deletePlace = async (req, res, next) => {
 
   if (!place) {
     const error = new HttpError("Could not find place for this id.", 404);
+    return next(error);
+  }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      "You are not allowed to delete this place.",
+      401
+    );
     return next(error);
   }
 
